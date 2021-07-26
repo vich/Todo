623f5d272054f7b313353a26fe3807403fbb3864
slava korenblit <3755214+vich@users.noreply.github.com>
2021-07-27 00:08:17 +0300

    Add EmailAddress attribute

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using TodoApi.Enums;
 
 namespace TodoApi.Models
@@ -6,12 +7,20 @@ namespace TodoApi.Models
     public class TodoItem
     {
         public long Id { get; set; }
+        
         public string Name { get; set; }
+        
         public DateTime DueDate { get; set; }
+        
         public Priority Priority { get; set; }
+        
         public Status Status { get; set; }
+        
         public string City { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string AssignTo { get; set; }
+        
         public string Secret { get; set; }
     }
 }

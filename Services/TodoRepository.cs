ac5dc77e18953c6a6d0d46f1d042e4683387a507
slava korenblit <3755214+vich@users.noreply.github.com>
2021-07-28 21:19:58 +0300

    Dispose

--- Services/TodoRepository.cs
+++ Services/TodoRepository.cs
@@ -98,6 +98,16 @@ namespace TodoApi.Services
 
         public void Dispose()
         {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // dispose resources when needed
+            }
         }
     }
 }

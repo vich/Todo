aebe440e04491bda543a913b005dec86de06234a
slava korenblit <3755214+vich@users.noreply.github.com>
2021-07-28 23:03:14 +0300

    Validate AssignTo

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using TodoApi.Enums;
 
 namespace TodoApi.Models
@@ -6,11 +7,21 @@ namespace TodoApi.Models
     public class TodoItemDTO
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public DateTime DueDate { get; set; }
+
+        [Required]
         public Priority Priority { get; set; }
+
+        [Required]
         public Status Status { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string AssignTo { get; set; }
     }
 }

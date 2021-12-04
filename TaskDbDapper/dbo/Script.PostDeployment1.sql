43fd19f538816d255980c4137987ac28ae2268cd
slava korenblit <3755214+vich@users.noreply.github.com>
2021-12-05 00:15:29 +0200

    Post deployment

---
+++ TaskDbDapper/dbo/Script.PostDeployment1.sql
@@ -0,0 +1,6 @@
+﻿if not exists (select 1 from dbo.[TodoItems])
+begin
+	insert into dbo.[TodoItems] (Id, Name, DueDate, Priority, Status, City, AssignTo)
+	values (CONVERT(uniqueidentifier, '36a12bfa-d2c4-4dc9-9906-eae63b4319a0'), 'Todo 1', CURRENT_TIMESTAMP, 2, 1, 'city1', 'a1@gmail.com'),
+		   (CONVERT(uniqueidentifier, '36a12bfa-1111-2222-3333-eae63b4319a0'), 'Todo 2', CURRENT_TIMESTAMP, 0, 0, 'city2', 'a2@gmail.com');
+end
